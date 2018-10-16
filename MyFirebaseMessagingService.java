a629c15a9a37e31f3c82a14500b91b0ec55c9973
MasudaTuba <41985315+MasudaTuba@users.noreply.github.com>
2018-10-17 04:05:25 +0600

    tuba

--- MyFirebaseMessagingService.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.projectandro;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
-}
