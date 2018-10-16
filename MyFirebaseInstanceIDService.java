6a04b9ddd4e95b7be9253da789e8ab7801b624b2
MasudaTuba <41985315+MasudaTuba@users.noreply.github.com>
2018-10-17 04:05:14 +0600

    tuba

--- MyFirebaseInstanceIDService.java
+++
@@ -1,6 +0,0 @@
-package com.example.user.projectandro;
-
-import com.google.firebase.iid.FirebaseInstanceIdService;
-
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
-}
